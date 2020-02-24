5d7e5e340ce00663848b566d3e8d9dadefa7c745
ericlrf <eric.lrf@gmail.com>
2020-02-24 04:17:17 -0300

    back to springfox 2.9.2 bugfix

--- src/main/java/com/apress/todo/TodoJdbcApplication.java
+++ src/main/java/com/apress/todo/TodoJdbcApplication.java
@@ -1,78 +1,12 @@
 package com.apress.todo;
 
-import java.util.Collections;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-
-import com.apress.todo.controller.UsuarioController;
-
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger.web.DocExpansion;
-import springfox.documentation.swagger.web.ModelRendering;
-import springfox.documentation.swagger.web.OperationsSorter;
-import springfox.documentation.swagger.web.TagsSorter;
-import springfox.documentation.swagger.web.UiConfiguration;
-import springfox.documentation.swagger.web.UiConfigurationBuilder;
-import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
 
 @SpringBootApplication
-@EnableSwagger2WebMvc
 public class TodoJdbcApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TodoJdbcApplication.class, args);
 	}
-	
-	@Bean
-	public Docket api() {
-		return new Docket(DocumentationType.SWAGGER_2)
-				.apiInfo(apiInfo())
-				.select()
-				.apis(RequestHandlerSelectors.any())
-				.paths(PathSelectors.any())
-				.build();
-				
-	}
-
-	private ApiInfo apiInfo() {
-		return new ApiInfo(
-		"My REST API",
-        "Some custom description of API.",
-        "API TOS",
-        "Terms of service",
-        new Contact("John Doe", "www.example.com", "myeaddress@company.com"),
-        "License of API",
-        "API license URL",
-        Collections.emptyList()
-        );
-	}
-	
-	UiConfiguration uiConfig() {
-		return UiConfigurationBuilder
-				.builder()
-				.deepLinking(true)
-				.displayOperationId(false)
-                .defaultModelsExpandDepth(1)
-                .defaultModelExpandDepth(1)
-                .defaultModelRendering(ModelRendering.EXAMPLE)
-                .displayRequestDuration(false)
-                .docExpansion(DocExpansion.NONE)
-                .filter(false)
-                .maxDisplayedTags(null)
-                .operationsSorter(OperationsSorter.ALPHA)
-                .showExtensions(false)
-                .tagsSorter(TagsSorter.ALPHA)
-                .supportedSubmitMethods(UiConfiguration.Constants.DEFAULT_SUBMIT_METHODS)
-                .validatorUrl(null)
-                .build();
-	}
-	
 }
